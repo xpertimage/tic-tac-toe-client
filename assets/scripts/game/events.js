abc4f06b4f1aedfa677b0b9c04500a6bc24b79e9
xpertimage <cox.ken@gmail.com>
2021-02-20 12:18:40 -0600

    Completed determining if there is a winner for the game

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -4,12 +4,28 @@ const ui = require('./ui')
 const store = require('../store')
 // Boolean variable used as a flag to switch between 'X' and 'O'
 let whosTurn = 'X'
+let myArray = []
+// checkForWinner is called with each move / click
+// to see if either party won with the last move
+const checkForWinner = function (arg) {
+  let myIndex
+  function isOccupiedByArg (element) {return element === arg}
+  for (let i=0; i<9; i++){
+    if (store.game.cells[i] === arg) {myArray.push(i)}
+    console.log('Arg= ' + arg)
+    console.log('Index ' + i + store.game.cells[i])
+  }
+  console.log('MyArray:' + myArray)
+  if (JSON.stringify(myArray) === JSON.stringify([0,1,2]) || JSON.stringify(myArray) === JSON.stringify([3,4,5])
+  || JSON.stringify(myArray) === JSON.stringify([6,7,8]) || JSON.stringify(myArray) === JSON.stringify([0,3,6])
+  || JSON.stringify(myArray) === JSON.stringify([1,4,7]) || JSON.stringify(myArray) === JSON.stringify([2,5,8])
+  || JSON.stringify(myArray) === JSON.stringify([0,4,8]) || JSON.stringify(myArray) === JSON.stringify([2,4,6]))
+   {
+     console.log ('Winner Winner Chicken Dinner!!')
+   }
+  myArray=[]
+  }
 
-// const checkForWinner = function (arg) {
-//   store.game.cells.forEach(element) {
-//   if (element === arg) {}
-// }
-// }
 
 const onSquareClick = function (event) {
   event.preventDefault()
@@ -21,6 +37,7 @@ const onSquareClick = function (event) {
     // Add the appropriate mark (X or O) to the correct index
     // in the Game Cells array.
     store.game.cells[event.target.id] = whosTurn
+    checkForWinner(whosTurn)
     // Alternate between 'X' and 'O'
     if (whosTurn === 'X') { whosTurn = 'O' } else { whosTurn = 'X' }
     // console.log(store.game.cells)
