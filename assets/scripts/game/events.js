3af175466ae5a68ac58fd8eede3a0e5b16a20344
xpertimage <cox.ken@gmail.com>
2021-02-20 05:47:27 -0600

    Added the functionality to place 'X' and 'O' on the game board. The user cannot change a square previously marked.

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -2,12 +2,15 @@ const api = require('./api')
 // require the ui file, so we can update the dom after an API request
 const ui = require('./ui')
 const store = require('../store')
+// Boolean variable used as a flag to switch between 'X' and 'O'
 let xORo = true
 
 const onSquareClick = function (event) {
   event.preventDefault()
-  console.log('SquareClick - Event=', event)
-  if ($(event.target).text('')) {
+  console.log(event.target)
+  // If the button/ square clicked has no text then place the appropriate
+  // mark in the square
+  if ($(event.target).text() === '') {
     if (xORo) {
       $(event.target).text('X')
       xORo = false
